6c2f9991cc247c32218a3bdbde6b3f91a7f989f1
Katarzyna Russek <86499265+FanFunKat@users.noreply.github.com>
2023-08-06 17:37:56 +0200

    added active dots

---
+++ exampless.js
@@ -0,0 +1,32 @@
+let myFirstFunction = () => {
+    console.log('Witaj! Mam na imię Katarzyna!');
+};
+
+let mySecondFunction = (name, greeating) => {
+    console.log(greeating + '! Mam na imię ' + name +'!');
+};
+
+mySecondFunction('Katarzyna', 'Hej');
+mySecondFunction('Kacper', 'Dzień dobry');
+
+// Napisz funkcję, która przemnoży liczbę przekazaną w parametrze przez dwa i wypisze wynik na konsolę.
+// Napisz funkcję, która przemnoży liczbę przekazaną w parametrze przez dowolną liczbę, przekazaną w kolejnym parametrze.
+// Napisz funkcję, która podniesie liczbę przekazaną w parametrze do potęgi drugiej.
+
+let multiplication = (number) => {
+    console.log(number * 2);
+};
+
+multiplication(5);
+
+let multiplication2 = (number1, number2) => {
+    console.log(number1 * number2);
+}
+
+multiplication2(125, 8);
+
+let exponentiation = (number) => {
+    console.log(number**2);
+}
+
+exponentiation(5);
